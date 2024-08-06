b03c6772b80763e8bb4ccb1b8ef05e226f41e319
KhalidOwlWalid <khalidowlwalid@gmail.com>
2024-08-06 16:29:35 +0100

    Add ability to change camera view

--- scene/main/main.gd
+++ scene/main/main.gd
@@ -1,34 +1,44 @@
 extends Node3D
 
+var player_camera: Camera3D = null
+		
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var player : CharacterBody3D = $CharacterBody3D
-	$MainCamera.current = true
 	# Check if player exists
 		
 	if player:
 		
 		var player_children: Array[Node] = player.get_children()
-		var player_camera: Camera3D = null
 		
 		for child in player_children:
 			print("[DEBUG] Child of player_children: ", child)
 			if child is Camera3D:
 				player_camera = child
-
+			
+		$MainCamera.make_current()
 		# Check if player camera exists or not
 		if player_camera:
 			player_camera.make_current()
-			$MainCamera.clear_current(false)
 			#$MainCamera.current = false
 			print("Using Player Camera")
 		else:
-			$MainCamera.make_current()
 			print("Player Camera3D not found.")
 			print("[DEBUG] player_camera returns: ", player_camera)
 	else:
-		$MainCamera.make_current()
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _input(event):
+	
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_C:
+			print("Changing camera view to CameraMain")
+			$MainCamera.make_current()
+		else:
+			print("Changing camera to PlayerCamera")
+			player_camera.make_current()
+			
